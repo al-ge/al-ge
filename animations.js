7a1256754a0ec1c58bf4208691a7af5e3d52f43b
al-ge <webmatrixa@gmail.com>
2019-03-28 20:56:54 -0700

    init

---
+++ animations.js
@@ -0,0 +1,17 @@
+// homepage
+ScrollReveal().reveal('.animateup', {delay: 100, distance: '20px', duration: 1000, reset: true})
+ScrollReveal().reveal('.material-icons', {delay: 100, duration: 2000, reset: true})
+ScrollReveal().reveal('.leftenter', {delay: 100, distance: '50%', origin: 'left', duration: 2000, reset: true})
+ScrollReveal().reveal('.rightenter', {delay: 100, distance: '50%', origin: 'right', duration: 2000, reset: true})
+ScrollReveal().reveal('.fadeanimate', {delay: 200, opacity: 0, duration: 3000, reset: true})
+
+ScrollReveal().reveal('#cloud_1', {delay: 200, opacity: .2, distance: '20%', origin: 'left', duration: 1500, reset: true})
+ScrollReveal().reveal('#cloud_2', {delay: 200, opacity: .4, distance: '50%', origin: 'left', duration: 1500, reset: true})
+
+
+
+// About
+ScrollReveal().reveal('#img_8_box', {delay: 2000, opacity: 0, duration: 3000, reset: true})
+
+
+// contact
